327dc7f4e14ab45d07d4692627ae8fc3fad4a29d
Greg Maxey <gregorymaxey@yahoo.com>
2020-09-23 17:15:54 +0100

    Skip component when version not supplied

--- No-artifact Application SSC.groovy
+++ No-artifact Application SSC.groovy
@@ -1,3 +1,16 @@
+/*
+
+CloudBees CD DSL: Self-service Catalog item to create an application model that updates environment inventory without an artifact version
+
+For legacy type deployments where artifacts are pushed to application servers, CloudBees CD uses the artifact version value to update environment inventory. However, when deployments are done by another tool, through an API or from a local command line you may still want to have CloudBees CD manage the deployment, updating inventory and performing other deployment tasks, for example. The generated application model can be used as the basis for these types of situations.
+
+The application model created from the self-service catalog item will have as many components as specified and will accept version numbers for each of those components in the deploy process. When run, the model will perform an empty deployment which updates the environment model with the components and version data. If no version is supplied for a particular component, the component deployment will be skipped.
+
+Note:
+- The deployment Stage Artifact option must be disabled for this model to work
+
+*/
+
 project 'Default'
 catalog 'No Artifact', {
   iconUrl = null
@@ -6,12 +19,12 @@ catalog 'No Artifact', {
   catalogItem 'No Artifact Application', {
     description = '''<xml>
   <title>
-    No deploy application model that updates environment inventory
+    No artifact application model that updates environment inventory
   </title>
 
   <htmlData>
     <![CDATA[
-      This item creates environments and an application model with components and component version input parameters. When run, the environment inventory is updated with the component version. No actual deployment takes place.<p>Smart Deploy and Artifact Staging must be disabled.</p>
+      This item creates environments and an application model with components and component version input parameters. When run, the environment inventory is updated with the component version. No actual deployment takes place. You can use this as a starting point for creating models that deploy using other mechanisms. Smart Deploy will examine the existing inventory and skip that component if the target version is already there. If no version is supplied, the component will be skipped<p>Artifact Staging must be disabled.</p>
     ]]>
   </htmlData>
 </xml>'''
@@ -22,7 +35,7 @@ catalog 'No Artifact', {
     "section": [{
       "name": "Application details",
       "instruction": "Provide details required to create the new application.",
-      "ec_parameterForm": "<editor> <formElement> <label>Application Name</label> <property>app</property> <documentation>Name of the application to be created.</documentation> <type>entry</type> <required>1</required> </formElement> <formElement> <label>Project Name</label> <property>projName</property> <documentation>Name of the application and environment project. e.g. \'Default\'</documentation> <type>project</type> <required>1</required> </formElement> <formElement> <label>Component Version List</label> <property>comps</property> <value>comp1=1.0</value><documentation>Name of the components and versions to be created. On component per line, componentname=version</documentation> <type>textarea</type> <required>1</required> </formElement><formElement> <label>Environment List</label> <property>envs</property> <documentation>Name of environments to be created, comma separated.  E.g., Dev,Int,QA</documentation> <type>entry</type> <required>1</required> </formElement></editor>"
+      "ec_parameterForm": "<editor> <formElement> <label>Application Name</label> <property>app</property> <documentation>Name of the application to be created.</documentation> <type>entry</type> <required>1</required> </formElement> <formElement> <label>Project Name</label> <property>projName</property> <documentation>Name of the application and environment project. e.g. \'Default\'</documentation> <type>project</type> <required>1</required> </formElement> <formElement> <label>Component Version List</label> <property>comps</property> <value>comp1=1.0\\ncomp2=2.0</value><documentation>Name of the components and versions to be created. On component per line, componentname=version</documentation> <type>textarea</type> <required>1</required> </formElement><formElement> <label>Environment List</label> <property>envs</property> <documentation>Name of environments to be created, comma separated.  E.g., Dev,Int,QA</documentation> <type>entry</type> <required>1</required> </formElement></editor>"
     }],
 	"endTarget": {
       "source": "form",
@@ -123,10 +136,19 @@ project proj,{
 
 			process \'Deploy\', {
 				processType = \'OTHER\'
-				formalParameter 'ec_smartDeployOption', defaultValue: '0'
 
 				components.each { comp, ver ->
 					formalParameter "${comp}_version", defaultValue: ver
+					
+					processStep 'No op', {
+						actualParameter = [
+							'commandToRun': 'echo',
+						]
+						applicationTierName = 'App Tier'
+						processStepType = 'command'
+						subprocedure = 'RunCommand'
+						subproject = '/plugins/EC-Core/project'
+					}					
 				
 					processStep comp, {
 						processStepType = \'process\'
@@ -135,6 +157,14 @@ project proj,{
 						subcomponentProcess = \'Install\'
 						applicationTierName = Tier
 					}
+					
+					processDependency 'No op', targetProcessStepName: comp, {
+						branchCondition = "\\$[/javascript myJob[\\"${comp}_version\\"]?true:false]"
+						branchConditionName = "Run ${comp}"
+						branchConditionType = 'CUSTOM'
+						branchType = 'ALWAYS'
+					}					
+					
 				} // each component
 			} // process
 				
